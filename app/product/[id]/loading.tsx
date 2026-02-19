5c0b74b6f76878a721ccc517532ccacf8e744e2d
Arum <arumrizkyr@gmail.com>
2026-02-19 11:30:30 +0700

    Add loading and error UI for SSR product detail page

---
+++ app/product/[id]/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Loading product...</h2>
+      <p>Please wait while we fetch product details.</p>
+    </div>
+  );
+}
